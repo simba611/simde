540890ff2a6c9d0ee0a29b6dcaebb74b4abb45ab
kunwar maheep <kunwar.maheep@students.iiit.ac.in>
2021-06-30 16:16:56 +0530

    minor formatting

--- simde/x86/avx512/shldv.h
+++ simde/x86/avx512/shldv.h
@@ -14,17 +14,17 @@ simde_mm_shldv_epi32(simde__m128i a, simde__m128i b, simde__m128i c) {
   #if defined(SIMDE_X86_AVX512VBMI2_NATIVE) && defined(SIMDE_X86_AVX512VL_NATIVE)
     return _mm_shldv_epi32(a, b, c);
   #else
-    simde__m128i_private
-      r_,
-      c_ = simde__m128i_to_private(c);
+    simde__m128i_private r_;
 
     #if defined(SIMDE_ARM_NEON_A64V8_NATIVE)
       simde__m128i_private
         a_ = simde__m128i_to_private(a),
-        b_ = simde__m128i_to_private(b);
+        b_ = simde__m128i_to_private(b),
+        c_ = simde__m128i_to_private(c);
 
-      uint64x2_t values_lo = vreinterpretq_u64_u32(vzip1q_u32(b_.neon_u32, a_.neon_u32));
-      uint64x2_t values_hi = vreinterpretq_u64_u32(vzip2q_u32(b_.neon_u32, a_.neon_u32));
+      uint64x2_t
+        values_lo = vreinterpretq_u64_u32(vzip1q_u32(b_.neon_u32, a_.neon_u32)),
+        values_hi = vreinterpretq_u64_u32(vzip2q_u32(b_.neon_u32, a_.neon_u32));
 
       int32x4_t count = vandq_s32(c_.neon_i32, vdupq_n_s32(31));
 
@@ -37,7 +37,6 @@ simde_mm_shldv_epi32(simde__m128i a, simde__m128i b, simde__m128i c) {
           vreinterpretq_u32_u64(values_hi)
         );
     #elif defined(SIMDE_X86_AVX2_NATIVE)
-      HEDLEY_STATIC_CAST(void, c_);
       simde__m256i
         tmp1,
         lo =
@@ -97,11 +96,9 @@ simde_mm_shldv_epi32(simde__m128i a, simde__m128i b, simde__m128i c) {
         );
     #elif defined(SIMDE_X86_SSE2_NATIVE)
       simde__m128i_private
-        lo,
-        hi;
-
-      lo = simde__m128i_to_private(simde_mm_unpacklo_epi32(b, a));
-      hi = simde__m128i_to_private(simde_mm_unpackhi_epi32(b, a));
+        c_ = simde__m128i_to_private(c),
+        lo = simde__m128i_to_private(simde_mm_unpacklo_epi32(b, a)),
+        hi = simde__m128i_to_private(simde_mm_unpackhi_epi32(b, a));
 
       size_t halfway = (sizeof(r_.u32) / sizeof(r_.u32[0]) / 2);
       SIMDE_VECTORIZE
@@ -122,7 +119,8 @@ simde_mm_shldv_epi32(simde__m128i a, simde__m128i b, simde__m128i c) {
     #else
       simde__m128i_private
         a_ = simde__m128i_to_private(a),
-        b_ = simde__m128i_to_private(b);
+        b_ = simde__m128i_to_private(b),
+        c_ = simde__m128i_to_private(c);
 
       SIMDE_VECTORIZE
       for (size_t i = 0 ; i < (sizeof(r_.u32) / sizeof(r_.u32[0])) ; i++) {
